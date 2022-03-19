32bc14ae115df85b610ee0fa87f5fe3501d90352
rereagri <89196875+rereagri@users.noreply.github.com>
2022-03-20 07:56:52 +0900

    implove

--- components/BlogList.vue
+++ components/BlogList.vue
@@ -2,7 +2,9 @@
   <div>
     <v-list v-if="blogs.length" class="overflow-y-auto">
       <v-list-item v-for="blog in blogs" :key="blog.id">
-        <blog-card ref="card" :blog="blog" @close="closeAll" />
+        <v-container class="py-1">
+          <blog-card ref="card" :blog="blog" @close="closeAll" />
+        </v-container>
       </v-list-item>
     </v-list>
     <span v-else>No Blogs...</span>
---
+++ components/MarkdownEditor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="markdown-editor">
+    <no-ssr>
+      <mavon-editor
+        v-model="model"
+        :toolbars="markdownOption"
+        language="ja"
+      />
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      type: Object | null,
+      default: null
+    }
+  },
+  data () {
+    return {
+      markdownOption: {
+        bold: true,
+        italic: true,
+        header: true,
+        underline: true,
+        strikethrough: true,
+        mark: true,
+        quote: true,
+        ol: true,
+        ul: true,
+        link: true,
+        imagelink: true,
+        code: true,
+        table: true,
+        fullscreen: true,
+        htmlcode: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .markdown-editor {
+    width: 100%;
+    height: 100%;
+  }
+</style>
